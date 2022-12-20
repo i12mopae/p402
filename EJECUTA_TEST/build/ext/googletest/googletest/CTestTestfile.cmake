c22410edf305712659e5c0363b7f1b3e7db69b82
i12semaa <118078046+i12semaa@users.noreply.github.com>
2022-12-20 23:26:22 +0100

    FINAL

---
+++ EJECUTA_TEST/build/ext/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/elena/myproyect/ext/googletest/googletest
+# Build directory: /home/elena/myproyect/build/ext/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
